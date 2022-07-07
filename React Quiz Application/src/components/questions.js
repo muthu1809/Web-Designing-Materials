556c89cf4e52655fc81ac96de8b2681f93d93225
Muthuramalingam <muthu1809@gmail.com>
2022-07-07 23:26:06 +0530

    Create questions.js

---
+++ React Quiz Application/src/components/questions.js
@@ -0,0 +1,22 @@
+export default[
+{question: "CM of Tamil Nadu ",
+options:["A", "B","C", "D"],
+answer: 0,
+},
+{question: "2nd ",
+options:["A", "B","C", "D"],
+answer: 0,
+},
+{question: "3rd ",
+options:["A", "B","C", "D"],
+answer: 3,
+},
+{question: "4th ",
+options:["A", "B","C", "D"],
+answer: 2,
+},
+{question: "5th ",
+options:["A", "B","C", "D"],
+answer: 1,
+}
+]
